b895a51a2582448dfb341dd32e5d0b69921914fc
Sanket Panda <48897257+SanketPanda@users.noreply.github.com>
2021-08-06 19:08:51 +0530

    initital commit

---
+++ controller.js
@@ -0,0 +1,161 @@
+console.log("Hello World!");
+var myObj;
+
+var active = document.querySelector(".active .no");
+var infected = document.querySelector(".infected .no");
+var recovered = document.querySelector(".recovered .no");
+var deaths = document.querySelector(".deaths .no");
+
+var activeDate = document.querySelector(".active .date");
+var infectedDate = document.querySelector(".infected .date");
+var recoveredDate = document.querySelector(".recovered .date");
+var deathsDate = document.querySelector(".deaths .date");
+
+var getJSON = function(url,callback)
+{
+ var https = new XMLHttpRequest();
+ https.open('GET',url,true);
+ https.onload = function(){
+     var status = https.status;
+     if(status === 200)
+     {
+         callback(null,https.response);
+     }
+     else
+     {
+         callback(status,https.response);
+     }
+ };
+ https.send();
+};
+
+var url = "https://api.covid19india.org/data.json";
+getJSON(url,function(err,data)
+{
+
+    if(err != null)
+    {
+        alert("Something went wrong! please reload the page");
+    }
+    else
+    {
+         myObj = JSON.parse(data);
+         console.log(myObj["statewise"]);
+    }
+});
+
+function stateData(countryData,selectedState)
+{
+    for(let i=0;i<countryData.statewise.length;i++)
+    {
+
+        if(selectedState === countryData.statewise[i].state)
+        {
+            console.log(selectedState+"   "+countryData.statewise[i].state);
+            active.innerHTML = countryData.statewise[i].active;
+            infected.innerHTML = countryData.statewise[i].confirmed;
+            recovered.innerHTML = countryData.statewise[i].recovered;
+            deaths.innerHTML = countryData.statewise[i].deaths;
+
+            activeDate.innerHTML = countryData.statewise[i].lastupdatedtime;
+            infectedDate.innerHTML = countryData.statewise[i].lastupdatedtime;
+            recoveredDate.innerHTML = countryData.statewise[i].lastupdatedtime;
+            deathsDate.innerHTML = countryData.statewise[i].lastupdatedtime;
+        }
+    }
+}
+
+const selected = document.querySelector(".choose-category .selected");
+const optionsContainer = document.querySelector(".choose-category .options-container");
+
+const optionsList = document.querySelectorAll(".choose-category .option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+
+});
+
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+    currentState = selected.textContent;
+    stateData(myObj,currentState);
+  });
+});
+
+
+const chartTypeSelected = document.querySelector(".chartCategory .selected");
+console.log(chartTypeSelected);
+console.log("Hello");
+const chartOptionsContainer = document.querySelector(".chart-options-container");
+const chartOptionsList = document.querySelectorAll(".chart-options-container .option");
+var currentChart;
+var selectedChart = 'bar';
+
+chartTypeSelected.addEventListener("click", () => {
+    chartOptionsContainer.classList.toggle("active");
+  
+  });
+
+  chartOptionsList.forEach(o => {
+    o.addEventListener("click", () => {
+    //chartTypeSelected.innerHTML = o.querySelector("label").innerHTML;
+    chartOptionsContainer.classList.remove("active");
+      currentChart = o.querySelector("label").innerHTML;
+      switch(currentChart){
+          case 'Line Chart':
+              console.log("Chart type is "+currentChart);
+            myChart.type = 'line';
+            selectedChart = 'line';
+            break;
+            case 'Bar Chart':
+            selectedChart = 'bar';
+            break; 
+            case 'Horizontal':
+            selectedChart = 'horizontal';
+            break;  
+            case 'Pie Chart':
+            selectedChart = 'pie';
+            break;   
+      }
+      console.log(currentChart)
+    });
+  });
+  
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: selectedChart,
+    data: {
+        labels: ['Active', 'Infected', 'Recovered', 'Deaths'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
